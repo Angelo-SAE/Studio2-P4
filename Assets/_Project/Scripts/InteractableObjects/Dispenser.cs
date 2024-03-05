b0fd2f96f8e04c7bc398b9aa90c542723508236b
Angelo <a.allaham@student.saedubai.com>
2024-03-05 17:16:51 +0400

    Added Feedback for Dispenser Buttons

--- Assets/_Project/Scripts/InteractableObjects/Dispenser.cs
+++ Assets/_Project/Scripts/InteractableObjects/Dispenser.cs
@@ -7,18 +7,35 @@ public class Dispenser : Interactable
 
     [SerializeField] private GameObject cupHolder;
     [SerializeField] private DrinkFlavors drinkFlavor;
+    [SerializeField] private Material buttonMat;
+    private bool coolDown;
 
     public override void Interact0(GameObject obj)
     {
       storedCup = cupHolder.GetComponent<Interactable>().StoredCup;
-      if(storedCup is not null)
+      if(storedCup is not null && !coolDown)
       {
         storedCup.GetComponent<Cup>().AddDrinkToMix((int)drinkFlavor);
+        coolDown = true;
+        ChangeMaterialColor(Color.red);
+        Invoke("CoolDownReset", 2f);
+
       }
     }
 
     public override void Interact1(GameObject obj)
     {
       //empty
+    }
+
+    private void ChangeMaterialColor(Color color)
+    {
+      buttonMat.color = color;
+    }
+
+    private void CoolDownReset()
+    {
+      coolDown = false;
+      ChangeMaterialColor(Color.green);
     }
 }
